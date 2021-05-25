ddbff3797dac002200f111a8ab03ccd11e58cd31
taylor-tokareff <taylortokareff@gmail.com>
2021-05-25 11:54:35 -0700

    page frame work

--- src/playlist/PlaylistPage.js
+++ src/playlist/PlaylistPage.js
@@ -2,12 +2,25 @@ import { Component } from 'react';
 import './PlaylistPage.css';
 
 export default class PlaylistPage extends Component {
-  
+
   render() {
+
     return (
       <div className="PlaylistPage">
-        
-      </div>
+        <form className="playListForm">
+          <h2>Choose your Theme</h2>
+          <p>
+            <label>
+              <span>Playlist</span>
+              <input className="playListForm" placeholder="Title of      Playlist" value=""
+              />
+            </label>
+          </p>
+
+
+        </form>
+
+      </div >
     );
   }
 
